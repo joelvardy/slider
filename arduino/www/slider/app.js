2500aee08107278216204a306378762a3b875f36
Joel Vardy <info@joelvardy.com>
2015-01-04 20:23:00 +0000

    Turn LED at digital 13 on or off through web

---
+++ arduino/www/slider/app.js
@@ -0,0 +1,34 @@
+Zepto(function ($) {
+
+	var sendRequest = function (endpoint) {
+
+		$.ajax({
+			type: 'GET',
+			username: 'root',
+			password: 'arduino',
+			url: 'http://arduino.local/arduino/'+endpoint
+		});
+
+	}
+
+	$('button.l13').click(function (e) {
+
+		if ($(this).hasClass('active')) {
+
+			sendRequest('digital/13/0');
+
+			$(this).html('Inactive');
+			$(this).removeClass('active');
+
+		} else {
+
+			sendRequest('digital/13/1');
+
+			$(this).html('Active');
+			$(this).addClass('active');
+
+		}
+
+	});
+
+});
--- readme.md
+++ readme.md
@@ -9,6 +9,13 @@ Password: s7fsEg4K
 
 Go to: http://arduino.cc/en/Tutorial/YunSysupgrade
 
+Install open SSH (for sFTP)
+
+```
+opkg update
+opkg install openssh-sftp-server
+```
+
 ## View Control Panel
 
 Go to: http://arduino.local/sd/slider
